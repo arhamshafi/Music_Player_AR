de1f419d5bf10109609b9fee9a52a3fb5d252211
arham <arhambutt2923@gmail.com>
2025-08-01 19:29:09 +0500

    setup is on working

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-red-400 w-full h-max py-0.1 '>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,35 @@
+
+export const artists = [
+    {
+        id: "artist1",
+        name: "Atif Aslam",
+        image: "https://i.scdn.co/image/ab6761610000e5eb6d3c62fdab4b7b716e3d10f3"
+    },
+    {
+        id: "artist2",
+        name: "Arijit Singh",
+        image: "https://i.scdn.co/image/ab6761610000e5ebeb0c9b622a4f78b4c12e6c27"
+    }
+];
+
+export const songs = [
+    {
+        id: "song1",
+        title: "Jeene Laga Hoon",
+        artistId: "artist1",
+        category: "Romantic",
+        image: "https://i.ytimg.com/vi/Nu5Im3gsRNk/maxresdefault.jpg",
+        audio: "/music/jeene.mp3"
+    },
+    {
+        id: "song2",
+        title: "Tum Hi Ho",
+        artistId: "artist2",
+        category: "Sad",
+        image: "https://i.ytimg.com/vi/Umqb9KENgmk/maxresdefault.jpg",
+        audio: "/music/tumhiho.mp3"
+    }
+];
+
+export const categories = ["Romantic", "Sad", "Workout"];
+
