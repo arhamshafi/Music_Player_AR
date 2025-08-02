23ca9ee4bb012e73560c165d98d68b78bda3e287
arham <arhambutt2923@gmail.com>
2025-08-03 00:02:19 +0500

    functionality added

--- src/context.jsx
+++ src/context.jsx
@@ -13,21 +13,52 @@ function AppProvider({ children }) {
     }
 
     let sing_handle = (e) => {
-        let {name , value} = e.target
-        set_signup_data({...sign_up_data , [name] : value})
+        let { name, value } = e.target
+        set_signup_data({ ...sign_up_data, [name]: value })
     }
 
     let login = () => {
-        console.log(loginData);
+        if (!loginData.email.trim() || !loginData.password.trim()) {
+            alert("Invalid Credentials")
+            return
+        }
+        let users = JSON.parse(localStorage.getItem("new_user") || [])
+        let matched_account = users.find((ele) => ele.email === loginData.email)
+        if (matched_account) {
+            if (matched_account.password === loginData.password) {
+                alert(`${matched_account.name} login successfully `)
+            }
+            else {
+                alert("Wrong password ! Try Again")
+            }
+        }
+        else {
+            alert("User Not Found")
+        }
+
     }
 
     let Sign_up = () => {
-        console.log(sign_up_data);
-        
+
+        if (!sign_up_data.name.trim() || !sign_up_data.email.trim() || !sign_up_data.number.trim() || !sign_up_data.password.trim()) {
+            alert(" All Fields Required To Process ")
+            return
+        }
+
+        let existingUsers = JSON.parse(localStorage.getItem("new_user")) || [];
+        let emailexist = existingUsers.find((ele) => ele.email === sign_up_data.email)
+        if (emailexist) {
+            alert("This Email is Already Exist")
+            return
+        }
+        existingUsers.push(sign_up_data);
+        localStorage.setItem("new_user", JSON.stringify(existingUsers));
+        set_signup_data({ name: "", email: "", password: "", number: "" })
+
     }
 
     return (
-        <App_context.Provider value={{ loginData, setLoginData, onhandle, login , sing_handle , sign_up_data , Sign_up }}>
+        <App_context.Provider value={{ loginData, setLoginData, onhandle, login, sing_handle, sign_up_data, Sign_up }}>
             {children}
         </App_context.Provider>
     );
